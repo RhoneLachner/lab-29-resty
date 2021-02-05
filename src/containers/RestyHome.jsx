ff387f2df6d76064a5abed26b68c649163742e5e
Rhone Lachner <fernandclay@gmail.com>
2021-02-04 17:54:57 -0800

    created file structure and imported Header to main container file

---
+++ src/containers/RestyHome.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Header from '../components/sectionView/Header';
+
+export default class RestyHome extends Component {
+  render() {
+    return (
+      <div>
+        <Header /> 
+      </div>
+    );
+  }
+}
+
