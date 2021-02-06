6c3b7d992a9b0d195a7e362ad0d2cc37681a9042
Rhone Lachner <fernandclay@gmail.com>
2021-02-05 17:24:50 -0800

    began adding text input to request file, radio inputs are next

--- src/components/sectionView/RequestView.jsx
+++ src/components/sectionView/RequestView.jsx
@@ -3,10 +3,19 @@ import React, { Component } from 'react';
 export default class RequestView extends Component {
   render() {
     return (
-      <div>
-        <h2>Request Section. 
+      <>
+        <div>
+          <h2>Request Section. 
             URL input and radio buttons here with GO button</h2>       
-      </div>
+        </div>
+
+        <form>
+          <input type="text" placeholder="http://api.url"></input>
+        </form>
+
+
+
+      </>
     );
   }
 }
