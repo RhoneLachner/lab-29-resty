aa8836ea6c562c496cbd8e2fcd61f293f5f2a30c
Rhone Lachner <fernandclay@gmail.com>
2021-02-07 21:45:23 -0800

    added prop types and added on Change plus checked to radio buttons

--- src/components/sectionView/RequestView.jsx
+++ src/components/sectionView/RequestView.jsx
@@ -1,37 +1,63 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import styles from '../../containers/RestyHome.css';
 
-export default class RequestView extends Component {
-  render() {
-    return (
-      <>
-        <div className={styles.requestView}>
+const RequestView = ({ radio, radioChange }) => (
+  <>
+    <div className={styles.requestView}>
 
-          <form>
-            <input type="text" placeholder="http://api.url.here"></input>
-            <button>GO!</button>
+      <form>
+        <input type="text" placeholder="http://api.url.here"></input>
+        <button>GO!</button>
+        {/* RADIO BUTTONS HERE */}
+        <div className={styles.radioButtons}>
 
-            {/* RADIO BUTTONS HERE */}
-            <div className={styles.radioButtons}>
+          <input className={styles.radio} 
+            type="radio" 
+            name="method" 
+            value="get" 
+            onChange={radioChange}
+            checked={radio === 'GET'}
+          />
+          <label htmlFor="get">GET</label>
 
-              <input type="radio" name="method" value="get" />
-              <label htmlFor="get">GET</label>
+          <input className={styles.radio} 
+            type="radio" 
+            name="method" 
+            value="post" 
+            onChange={radioChange}
+            checked={radio === 'POST'}
+          />
+          <label htmlFor="get">POST</label>
 
-              <input type="radio" name="method" value="post" />
-              <label htmlFor="get">POST</label>
+          <input className={styles.radio} 
+            type="radio" 
+            name="method" 
+            value="put" 
+            onChange={radioChange}
+            checked={radio === 'PUT'}
+          />
+          <label htmlFor="get">PUT</label>
 
-              <input type="radio" name="method" value="put" />
-              <label htmlFor="get">PUT</label>
+          <input className={styles.radio} 
+            type="radio" 
+            name="method" 
+            value="delete" 
+            onChange={radioChange}
+            checked={radio === 'DELETE'}
+          />
+          <label htmlFor="get">DELETE</label>
 
-              <input type="radio" name="method" value="delete" />
-              <label htmlFor="get">DELETE</label>
-
-            </div>
-
-          </form>
         </div>
-      </>
-    );
-  }
-}
+      </form>
+    </div>
+  </>
+);
+
+RequestView.propTypes = {
+  radio: PropTypes.string.isRequired,
+  radioChange: PropTypes.func.isRequired
+};
+
+export default RequestView;
 
